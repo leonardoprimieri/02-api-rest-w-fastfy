dfa0e3bda6e740f670a96e78ae4edc4ce29befb6
Leonardo M. Primieri <leo.primieri@gmail.com>
2023-02-09 09:53:29 -0300

    chore: add and validate env vars

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,14 @@
+import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined')
+}
 
 export const knexConfig: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: String(process.env.DATABASE_URL),
   },
   useNullAsDefault: true,
   migrations: {
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { randomUUID } from 'node:crypto'
 
 const app = fastify()
 
 app.get('/', async () => {
-  return await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+    .insert({
+      id: randomUUID(),
+      title: 'Test transaction',
+      amount: 1000,
+    })
+    .returning('*')
+  return transaction
 })
 
 app
