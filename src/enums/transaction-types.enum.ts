d976c638fb7b957d116daf29a90d1ff8951015d1
Leonardo M. Primieri <leo.primieri@gmail.com>
2023-02-09 10:53:01 -0300

    feat: type table on knex schema

---
+++ src/enums/transaction-types.enum.ts
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+export enum TransactionTypesEnum {
+  CREDIT = 'credit',
+  DEBIT = 'debit',
+}
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,16 +1,27 @@
 import { randomUUID } from 'node:crypto'
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
+import { TransactionTypesEnum } from '../enums/transaction-types.enum'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transaction = await knex('transactions')
-      .insert({
-        id: randomUUID(),
-        title: 'Test transaction',
-        amount: 1000,
-      })
-      .returning('*')
-    return transaction
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.nativeEnum(TransactionTypesEnum),
+    })
+
+    const { amount, title, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      amount: type === TransactionTypesEnum.CREDIT ? amount : amount * -1,
+      title,
+    })
+
+    return reply.status(201).send()
   })
 }
