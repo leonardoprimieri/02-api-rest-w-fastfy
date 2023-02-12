1af19f63c42825b35a42dd9fe687f0e829a7a61b
Leonardo M. Primieri <leo.primieri@gmail.com>
2023-02-12 18:36:28 -0300

    test: ensure transactions will be listed

--- src/tests/transactions.spec.ts
+++ src/tests/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, describe, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import request from 'supertest'
 import { app } from '../app'
 import { TransactionTypesEnum } from '../enums/transaction-types.enum'
@@ -21,5 +21,29 @@ describe('Transactions Service', () => {
         type: TransactionTypesEnum.CREDIT,
       })
       .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 500,
+        type: TransactionTypesEnum.CREDIT,
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 500,
+      }),
+    ])
   })
 })
