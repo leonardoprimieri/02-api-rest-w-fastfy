69879808b697fca2cdfb4df54b19211ea6b9113a
Leonardo M. Primieri <leo.primieri@gmail.com>
2023-02-12 11:48:59 -0300

    feat: create summary and by id routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,6 +5,31 @@ import { z } from 'zod'
 import { TransactionTypesEnum } from '../enums/transaction-types.enum'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return { summary }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
@@ -16,9 +41,13 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body,
     )
 
+    const handleAmount = () => {
+      return type === TransactionTypesEnum.CREDIT ? amount : amount * -1
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
-      amount: type === TransactionTypesEnum.CREDIT ? amount : amount * -1,
+      amount: handleAmount(),
       title,
     })
 
